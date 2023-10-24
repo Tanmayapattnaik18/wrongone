749bc81855e2bdef85776cd83306c6bcf9ad678b
Tanmayapattnaik18 <tanmaya.pattanaik18@gmail.com>
2023-10-24 12:11:39 +0530

    correct it

---
+++ p44.js
@@ -0,0 +1,12 @@
+// Problem 2: Create a new array from the given array such that each element of the new array is raised to a power of 5.
+// Input = [2, 3, 4, 6, 7]
+// Output = [32, 243, 1024, 7776, 16807]
+
+let a= [2, 3, 4, 6, 7];
+let s=[];
+a.forEach((element)=>{
+  s.push(element**5);
+})
+console.log();
+
+
